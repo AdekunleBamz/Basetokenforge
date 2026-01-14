e0d8c0f8e5cb92a6b5530ca70fc6991054d7bb80
SmartFlow Developer <adekunle@smartflow.dev>
2026-01-14 19:43:43 +0100

    feat(utils): add barrel export for all utilities

---
+++ frontend/src/lib/utils/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Utils barrel export
+ */
+
+export * from './string';
+export * from './number';
+export * from './date';
+export * from './validation';
+export * from './cn';
+export * from './storage';
+export * from './clipboard';
+export * from './async';
