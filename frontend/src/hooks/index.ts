9c0d0d030f19b800cfdc34ab04b1d4716717f764
SmartFlow Developer <adekunle@smartflow.dev>
2026-01-14 20:01:34 +0100

    feat(hooks): add barrel export for all custom hooks

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Custom hooks barrel export
+ */
+
+export { useFarcaster } from './useFarcaster';
+export { useCopy } from './useCopy';
+export { useLocalStorage } from './useLocalStorage';
+export { useDebounce, useDebouncedCallback } from './useDebounce';
+export { useDisclosure } from './useDisclosure';
+export { useWindowSize, useIsMobile, useIsTablet, useIsDesktop, useBreakpoint } from './useWindowSize';
+export { useClickOutside } from './useClickOutside';
+export { useKeyboard, useEscapeKey, useEnterKey } from './useKeyboard';
+export { useMounted, useIsClient } from './useMounted';
+export { usePrevious, useHasChanged } from './usePrevious';
+export { useWallet } from './useWallet';
+export { useTokenCreator } from './useTokenCreator';
+export { useUserTokens } from './useUserTokens';
