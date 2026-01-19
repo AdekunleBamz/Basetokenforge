d2059ea926d7874419c57b6d50825be83643eb7d
SmartFlow Developer <adekunle@smartflow.dev>
2026-01-19 18:30:57 +0100

    chore(context): add barrel export for contexts

---
+++ frontend/src/context/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Context Barrel Export
+ * 
+ * Central export for all app contexts.
+ */
+
+export { CreationFlowProvider, useCreationFlow } from './CreationFlowContext';
+export { NetworkProvider, useNetwork, useBasescanUrl } from './NetworkContext';
+export { TokenProvider, useTokenContext, useRecentTokens } from './TokenContext';
